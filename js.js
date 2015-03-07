f3200296b8858e58a09de1fd7b5b8444054dd0bd
Chris Ernst <cernst3@gatech.edu>
2015-03-07 15:31:08 -0500

    instruments
    Added instruments abstraction. Better antislack value. Force audio
    context on mobile.

--- js.js
+++ js.js
@@ -1,35 +1,22 @@
 var detuneMultipliers = [-1,1];
-var context = new AudioContext();
+var context = new webkitAudioContext();
 var tonic;
 var cents;
 var freqs = [440, 466.164, 493.883, 523.252, 554.366, 293.665, 311.127, 329.628, 349.228, 369.994, 391.995, 415.305];
 var notes = ["A", "A#/Bb", "B",     "C",     "C#/Db", "D",     "D#/Eb", "E",     "F",     "F#/Gb", "G",     "G#"];
 var detuneMultiplier;
-var antislack = .5;
+var antislack = .9;
 var slack = 25;
 
 //timbre
-var real = new Float32Array(9);
-real[0] = 0;
-real[1] = 1;
-real[2] = 2;
-real[3] = 4;
-real[4] = 8;
-real[5] = 16;
-real[6] = 32;
-real[7] = 64;
-real[8] = 128;
-var img = new Float32Array(9);
-img[0] = 0;
-img[1] = 1;
-img[2] = 2;
-img[3] = 4;
-img[4] = 8;
-img[5] = 16;
-img[6] = 32;
-img[7] = 64;
-img[8] = 128;
-var myInstrument = context.createPeriodicWave(real, img);
+var instrument = organ;
+var real = new Float32Array(instrument.real.length);
+var imag = new Float32Array(instrument.imag.length);
+for (var i = 0; i < instrument.real.length; i++) {
+  real[i] = instrument.real[i];
+  imag[i] = instrument.imag[i];
+}
+var myInstrument = context.createPeriodicWave(real, imag);
 
 $(function(){
 	if(localStorage.getItem("bestCents")){
@@ -115,7 +102,7 @@ function guessedLower(){
 		if(localStorage.getItem("bestCents")>Math.abs(cents)){
 			localStorage.setItem("bestCents", Math.abs(cents));
 		}
-		cents = cents*.5;
+		cents = cents*antislack;
 	}
 	else{
 		//incorrect
@@ -131,7 +118,7 @@ function guessedHigher(){
 		if(localStorage.getItem("bestCents")>Math.abs(cents)){
 			localStorage.setItem("bestCents", Math.abs(cents));
 		}
-		cents = cents*.5;
+		cents = cents*antislack;
 	}
 	else{
 		//incorrect
